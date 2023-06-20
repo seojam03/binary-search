84e2a27eb8e20cf76d0bfa84d12e1c3a0205fd58
seojam03 <131682547+seojam03@users.noreply.github.com>
2023-06-20 22:58:59 +0900

    2

--- 백준 2805(나무 자르기).c
+++ 백준 2805(나무 자르기).c
@@ -8,7 +8,9 @@ int n, m, v[1'000'000];
 bool Check(const int mid) { // mid 높이에 절단기를 위치했을 때 m 이상의 나무를 얻을 수 있는가?
     long long sum = 0; // 오버플로우 조심
     for (int i = 0; i < n; i++) {
-        if (v[i] > mid) sum += v[i] - mid;
+        if (v[i] > mid) {
+            sum += v[i] - mid;
+        }
     }
     return sum >= m;
 }
@@ -16,8 +18,10 @@ bool Check(const int mid) { // mid 높이에 절단기를 위치했을 때 m 이
 int main() {
     fastio;
     cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> v[i];
-
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    
     // 이분 탐색
     int lo = 0, hi = 1e9;
     // Checklist
@@ -26,7 +30,9 @@ int main() {
 
     while (lo + 1 < hi) { // lo와 hi 사이에 다른 칸이 존재하는가?
         int mid = (lo + hi) / 2; // 항상 lo < mid < hi를 만족 (평균을 생각해보면 o)
-        if (Check(mid)) lo = mid;
+        if (Check(mid)) {
+            lo = mid;
+        }
         else hi = mid;
     }
     cout << lo << '\n';
